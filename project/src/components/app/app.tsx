d06dc62bdf2a7218a3cbbf5126032f9bdad169b7
DariaArtuhova <darianomoko@yandex.ru>
2022-10-20 23:35:18 +0300

    добавила компоненты

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,13 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import {Main} from '../../pages/main-page/main';
+
+type AppCardsProps = {
+  cardsCount: number;
+}
+
+function App({cardsCount}: AppCardsProps): JSX.Element {
+  return (
+    <Main cardsCount={cardsCount}/>
+  );
 }
 
 export default App;
