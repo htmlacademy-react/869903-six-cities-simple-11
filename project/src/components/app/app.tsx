ea02eebcd26b497ae87013688eb9d595ab294124
DariaArtuhova <darianomoko@yandex.ru>
2022-10-25 10:26:09 +0300

    добавила маршрутизацию

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,9 @@
 import {Main} from '../../pages/main-page/main';
+import {Login} from '../../pages/login-page/login';
+import {Room} from '../../pages/room-page/room';
+import {Error} from './error';
 import {OfferType} from '../../pages/mock/offers';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 type AppCardsProps = {
   offers: OfferType[];
@@ -7,7 +11,14 @@ type AppCardsProps = {
 
 function App({offers}: AppCardsProps): JSX.Element {
   return (
-    <Main offers={offers}/>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Main offers={offers}/>} />
+        <Route path='login' element={<Login />} />
+        <Route path='room' element={<Room />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
